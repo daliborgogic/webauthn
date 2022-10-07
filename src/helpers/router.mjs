6dd0da40cadd52d947f5b779a462015e235bf003
dalibor <mail@daliborgogic.com>
2022-10-07 18:01:09 +0200

    router and routes

---
+++ src/helpers/router.mjs
@@ -0,0 +1,31 @@
+export default function Router({ base = '', routes = [] } = {}) {
+  return {
+    __proto__: new Proxy({}, {
+      get: (_, prop, receiver) => (route, ...handlers) =>
+        routes.push([
+          prop.toUpperCase(),
+          RegExp(`^${(base + route)
+            .replace(/(\/?)\*/g, '($1.*)?')                             // trailing wildcard
+            .replace(/(\/$)|((?<=\/)\/)/, '')                           // remove trailing slash or double slash from joins
+            .replace(/:(\w+)(\?)?(\.)?/g, '$2(?<$1>[^/]+)$2$3')         // named params
+            .replace(/\.(?=[\w(])/, '\\.')                              // dot in path
+            .replace(/\)\.\?\(([^\[]+)\[\^/g, '?)\\.?($1(?<=\\.)[^\\.') // optional image format
+          }/*$`),
+          handlers,
+        ]) && receiver
+    }),
+    routes,
+    async handle (request, ...args) {
+      let response, match, url = new URL(request.url)
+      request.query = Object.fromEntries(url.searchParams)
+      for (let [method, route, handlers] of routes) {
+        if ((method === request.method || method === 'ALL') && (match = url.pathname.match(route))) {
+          request.params = match.groups
+          for (let handler of handlers) {
+            if ((response = await handler(request.proxy || request, ...args)) !== undefined) return response
+          }
+        }
+      }
+    }
+  }
+}
--- src/index.mjs
+++ src/index.mjs
@@ -1,9 +1,21 @@
+import Router from './helpers/router.mjs'
+
+const router = Router()
+
+router.get('/registration', () => {})
+router.get('/authentication', () => {})
+
+router.post('/verify-registration', () => {})
+router.post('/verify-authentication', () => {})
+
 export default {
-  fetch () {
-    try {
-      return Response.json({ status: 200 })
-    } catch (error) {
-      return Response.json({ error: error.message} , { status: 500 })
-    }
-  }
+  fetch: (...args) => router
+    .handle(...args)
+    .then(response => {
+      // can modify response here before final return, e.g. CORS headers
+      return response
+    })
+    .catch(err => {
+      // and do something with the errors here, like logging, error status, etc
+    })
 }
